c45ff82f497a612daf8469ede9f3e8069a5298e6
mikata <h_mika22@bg8.so-net.ne.jp>
2023-07-24 22:29:15 +0900

    default.php追加。jsを読み込まないとツールバーが出ないので編集できるように変更。グローバルヘッダとフッターとメインのみ。

---
+++ packages/theme_mika/themes/theme_mika/default.php
@@ -0,0 +1,33 @@
+<?php
+defined('C5_EXECUTE') or die("Access Denied.");
+use Concrete\Core\Area\GlobalArea;
+?>
+<!DOCTYPE html>
+<head>
+    <?php
+    View::element('header_required', [
+        'pageTitle' => isset($pageTitle) ? $pageTitle : '',
+        'pageDescription' => isset($pageDescription) ? $pageDescription : '',
+        'pageMetaKeywords' => isset($pageMetaKeywords) ? $pageMetaKeywords : ''
+    ]);
+    ?>
+</head>
+<body>
+<?php
+
+$a = new GlobalArea('Navigation');
+$a->display();
+
+$a = new Area('Main');
+$a->display($c);
+
+$a = new GlobalArea('Navigation');
+$a->display();
+
+?>
+<?php View::element('footer_required'); ?>
+<script type="text/javascript" src="<?=$view->getThemePath()?>/main.js"></script>
+
+</body>
+</html>
+
