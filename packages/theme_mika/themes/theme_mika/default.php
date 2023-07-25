6b4f67c6c446fe3264b3daffa7b8e3979e1f3dd9
mikata <h_mika22@bg8.so-net.ne.jp>
2023-07-25 16:54:12 +0900

    globalをheader, footerに移動

--- packages/theme_mika/themes/theme_mika/default.php
+++ packages/theme_mika/themes/theme_mika/default.php
@@ -1,16 +1,9 @@
 <?php
+defined('C5_EXECUTE') or die("Access Denied.");
 
 $view->inc('elements/header.php');
 
-defined('C5_EXECUTE') or die("Access Denied.");
-
-$a = new GlobalArea('header global');
-$a->display();
-
 $a = new Area('Main');
 $a->display($c);
-
-$a = new GlobalArea('footer global');
-$a->display();
 
 $view->inc('elements/footer.php');
