ff959624d4e0f034283fed1bf333b4c562d24429
mikata <h_mika22@bg8.so-net.ne.jp>
2023-07-25 15:53:11 +0900

    toolbarのマージンを追加

--- packages/theme_mika/themes/theme_mika/default.php
+++ packages/theme_mika/themes/theme_mika/default.php
@@ -1,6 +1,8 @@
 <?php
 defined('C5_EXECUTE') or die("Access Denied.");
 use Concrete\Core\Area\GlobalArea;
+$c = Page::getCurrentPage();
+$p = new Permissions($c);
 ?>
 <!DOCTYPE html>
 <head>
@@ -15,10 +17,9 @@ use Concrete\Core\Area\GlobalArea;
     ]);
     ?>
 
-
-
 </head>
-<body>
+<body <?= $p->canViewToolbar() ? 'style="margin-top:47px"':''?>>
+<div>
 <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
   <symbol id="home" viewBox="0 0 16 16">
     <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z"></path>
@@ -37,6 +38,7 @@ $a = new GlobalArea('footer global');
 $a->display();
 
 ?>
+</div>
 <?php View::element('footer_required'); ?>
 <script type="text/javascript" src="<?=$view->getThemePath()?>/main.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
