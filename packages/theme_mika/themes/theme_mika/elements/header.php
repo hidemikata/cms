142f4f38c3134c7f8b2e846110dd2645391fadae
mikata <h_mika22@bg8.so-net.ne.jp>
2023-07-28 16:36:41 +0900

    modified

--- packages/theme_mika/themes/theme_mika/elements/header.php
+++ packages/theme_mika/themes/theme_mika/elements/header.php
@@ -5,6 +5,7 @@ $c = Page::getCurrentPage();
 $p = new Permissions($c);
 ?>
 <!DOCTYPE html>
+<html lang="ja">
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
