cd7d8bf8a2773b3e0a658903036107f755419fc5
mikata <h_mika22@bg8.so-net.ne.jp>
2023-07-31 16:33:25 +0900

    modified.

--- packages/theme_mika/themes/theme_mika/top_content.php
+++ packages/theme_mika/themes/theme_mika/top_content.php
@@ -6,8 +6,16 @@ $view->inc('elements/header.php');
 //container for grid
 ?>
 <div class="container top-content-body">
+
+
+
 <div class="row">
-<div class="col-sm">
+
+<div class="col-sm-3 top-content-left">
+contents
+
+</div>
+<div class="col-sm-9 top-content-main">
 <?php
 $a = new Area('blog');
 $a->display($c);
