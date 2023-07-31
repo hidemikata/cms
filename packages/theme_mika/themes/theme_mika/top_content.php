89c8cc9330ce460b65fe6bb365a8f6f13125e76c
mikata <h_mika22@bg8.so-net.ne.jp>
2023-07-31 15:09:34 +0900

    ブログコンテンツのテンプレートを追加

---
+++ packages/theme_mika/themes/theme_mika/top_content.php
@@ -0,0 +1,19 @@
+<?php
+defined('C5_EXECUTE') or die("Access Denied.");
+
+$view->inc('elements/header.php');
+
+//container for grid
+?>
+<div class="container top-body">
+<div class="row">
+<div class="col-sm">
+<?php
+$a = new Area('blog');
+$a->display($c);
+?>
+</div>
+</div>
+</div>
+<?php
+$view->inc('elements/footer.php');
