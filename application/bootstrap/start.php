4f51733e429fd073f1d65bfb03e698f8ab56cffe
mikata <h_mika22@bg8.so-net.ne.jp>
2023-08-01 13:24:17 +0900

    add no cache

--- application/bootstrap/start.php
+++ application/bootstrap/start.php
@@ -8,7 +8,9 @@ use Concrete\Core\Application\Application;
  * ----------------------------------------------------------------------------
  */
 $app = new Application();
-
+//暫定
+header( 'Cache-Control: no-store, no-cache, must-revalidate' );
+header( 'Cache-Control: post-check=0, pre-check=0', FALSE );
 /*
  * ----------------------------------------------------------------------------
  * Detect the environment based on the hostname of the server
