da85378e1ce653f965849b69a1d5f80da473653c
mikata <h_mika22@bg8.so-net.ne.jp>
2023-08-04 13:26:39 +0900

    不要コード削除

--- packages/theme_mika/themes/theme_mika/elements/header.php
+++ packages/theme_mika/themes/theme_mika/elements/header.php
@@ -16,7 +16,6 @@ $p = new Permissions($c);
   <?php
   $collentionName = $c->getCollectionName(); //seoのページ名
   $pageTitle = $collentionName . '|' . 'ConcreteCMSで鹿';
-  Loader::element('header_required', array('pageTitle' => $pageTitle));
   ?>
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
