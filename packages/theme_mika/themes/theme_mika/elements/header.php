712dc8d0fe15f70340b105919ede175befeb5a0e
mikata <h_mika22@bg8.so-net.ne.jp>
2023-08-04 09:08:28 +0900

    ページのタイトルをカスタマイズ

--- packages/theme_mika/themes/theme_mika/elements/header.php
+++ packages/theme_mika/themes/theme_mika/elements/header.php
@@ -9,6 +9,13 @@ $p = new Permissions($c);
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php
+$collentionName = $c->getCollectionName();//seoのページ名
+$pageTitle = $collentionName . '|' . 'ConcreteCMSで鹿';
+Loader::element('header_required', array('pageTitle' => $pageTitle));
+?>
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 <?php
 //デフォルトで読み込み（style.css）
