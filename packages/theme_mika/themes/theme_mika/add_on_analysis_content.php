aa1dbfb8bae461a64da79621e5671a762f7409d7
hidemikata <h_mika22@bg8.so-net.ne.jp>
2023-08-04 21:03:34 +0900

    アドオン解説ブログのページテンプレートを作成

---
+++ packages/theme_mika/themes/theme_mika/add_on_analysis_content.php
@@ -0,0 +1,22 @@
+<?php
+defined('C5_EXECUTE') or die("Access Denied.");
+
+$view->inc('elements/header.php');
+
+//container for grid
+?>
+<div class="container top-content-body">
+
+
+
+    <div class="row">
+        <div class="col-sm top-content-main">
+            <?php
+            $a = new Area('blog');
+            $a->display($c);
+            ?>
+        </div>
+    </div>
+</div>
+<?php
+$view->inc('elements/footer.php');
