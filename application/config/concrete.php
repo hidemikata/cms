73976e1419fd5ed21f7f99a3b01023bde636a3d5
hidemikata <h_mika22@bg8.so-net.ne.jp>
2023-08-21 16:24:46 +0900

    ログのファイル出力設定

--- application/config/concrete.php
+++ application/config/concrete.php
@@ -5,4 +5,38 @@ return [
             'invalidate_on_ip_mismatch' => false,
         ],
     ],
+    'log' => [
+        'configuration' => [
+            /*
+             * Configuration mode
+             *
+             * @var string simple|advanced
+             */
+            'mode' => 'simple',
+            'simple' => [
+                /*
+                 * What log level to store core logs in the database
+                 * @var string
+                 */
+                'core_logging_level' => 'DEBUG',
+
+                /*
+                 * Which handle to use
+                 *
+                 * @var string (database|file)
+                 */
+                'handler' => 'file',
+
+                'file' => [
+                    /*
+                     * File path to store logs
+                     *
+                     * @var string
+                     */
+                    'file' => '/var/log/concretecms/concretecms.log',
+                ],
+            ],
+
+        ]
+    ]
 ];
