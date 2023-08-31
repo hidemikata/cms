b397b68f96d0c5a9e58af31c45ddc39a783d2d77
hidemikata <h_mika22@bg8.so-net.ne.jp>
2023-08-31 16:07:42 +0900

    coreを上がいてDIできるようにbindを追記

--- packages/theme_mika/src/MikaLogger/MikaServiceProvider.php
+++ packages/theme_mika/src/MikaLogger/MikaServiceProvider.php
@@ -10,10 +10,15 @@ class MikaServiceProvider extends ServiceProvider
 {
     public function register()
     {
+
         //ロガーなのでシングルトン
         $this->app->singleton('mikalog', function ($app) {
             $mika_logger = new MikaLogger();
             return $mika_logger->getLogger();
         });
+
+        $this->app->bind('mikalogger', 'mikalog');
+        $this->app->bind('Psr\Log\LoggerInterface', 'mikalog');
+        $this->app->bind('Concrete\Core\Logging\Logger', 'mikalog');
     }
 }
