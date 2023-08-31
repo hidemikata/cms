32629b9ef393a81d6a1ce8c3be23c28b7f29494d
hidemikata <h_mika22@bg8.so-net.ne.jp>
2023-08-31 16:09:33 +0900

    DIついかしてみた

--- packages/theme_mika/controllers/api_test.php
+++ packages/theme_mika/controllers/api_test.php
@@ -3,19 +3,32 @@
 namespace Concrete\Package\ThemeMika\Controller;
 
 use Controller;
+use User;
+use MikaSrc\MikaLogger\MikaLogger;
+use Psr\Log\LoggerInterface;
 
 class ApiTest extends Controller
 {
     protected $viewPath = '/api_test_view';
 
+    private $logger;
+    public function __construct(User $user, LoggerInterface $logger)
+    {
+        //dipendency injectionも可能ぽい
+        $this->logger = $logger;
+    }
+
     public function test($param1)
     {
+        $this->logger->debug('test');
         var_dump('test');
         $this->set('foo', $param1);
     }
 
     public function test2($param2)
     {
+        $this->logger->debug($param2);
+
         var_dump('test');
         $this->set('foo', $param2);
     }
