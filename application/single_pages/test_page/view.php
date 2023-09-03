45c04191a07805fac036b2f2a46cc1f62c28c069
hidemikata <h_mika22@bg8.so-net.ne.jp>
2023-09-03 14:27:40 +0900

    テストようシングルページ追加

---
+++ application/single_pages/test_page/view.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<body>this is single page.
+    <br>
+    ローカル画像
+    <br>
+    <img src="/application/files/6116/9371/7780/IMG_0432.JPG" width="100" height="100">
+
+    <br>
+    cloudfront画像
+    <br>
+    <img src="https://d12po91z9cv0gt.cloudfront.net/img/IMG_0021.JPG" width="100" height="100">
+
+
+</body>
+
+</html>
