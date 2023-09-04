6115b3fdef0c60be0e7c5decf43a3bb98ec34b6d
hidemikata <h_mika22@bg8.so-net.ne.jp>
2023-09-04 09:57:32 +0900

    cloudfrontテスト。画像をクラウドフロントから取得。

--- application/single_pages/test_page/view.php
+++ application/single_pages/test_page/view.php
@@ -5,12 +5,12 @@
     <br>
     ローカル画像
     <br>
-    <img src="/cms/test.jpg" width="100" height="100">
+    <img src="https://d262z4734lyo9h.cloudfront.net/cms/test.jpg" width="100" height="100">
 
     <br>
     cloudfront画像
     <br>
-    <img src="https://d12po91z9cv0gt.cloudfront.net/img/IMG_0021.JPG" width="100" height="100">
+    <img src="https://d262z4734lyo9h.cloudfront.net/s3img/IMG_0432.JPG" width="100" height="100">
 
 
 </body>
