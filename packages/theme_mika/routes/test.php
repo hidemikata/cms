30b90a3d87891b2a7c5798158978e3892adefccd
hidemikata <h_mika22@bg8.so-net.ne.jp>
2023-09-06 16:05:05 +0900

    modified

--- packages/theme_mika/routes/test.php
+++ packages/theme_mika/routes/test.php
@@ -2,5 +2,6 @@
 defined('C5_EXECUTE') or die('Access Denied.');
 
 // '/api' prefix
+$router->all('/api_test/test/{param}', 'ApiTest::test');
 $router->all('/api_test/test2/{param2}', 'ApiTest::test2');
 $router->all('/api_test/config_test', 'ApiTest::config_test');
